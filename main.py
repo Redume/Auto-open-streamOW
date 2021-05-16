d7a2238a375ac1f7458a33279ffa235202adcdb5
Koteich <49362257+Koteich-dev@users.noreply.github.com>
2021-05-16 13:15:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,37 @@ class Overwatch:
 
 
 	def contenders_parsing(self):
+	try:
+		options = Options()
+		options.headless = True
+		driver = webdriver.Firefox(options=options, executable_path=r'C:\webDrivers\geckodriver.exe')
+		driver.get(self.contenders)
+
+		full_page = driver.page_source
+		soup = BeautifulSoup(full_page, "html.parser")
+		items = soup.find_all("h2", {"class": "section-titlestyles__Title-sc-1wlp19u-3", "class": "bJDlju"})
+
+		if items[0].text == "Live now":
+			Logger.info("Стрим Overwatch Contenders найден! Открываю страничку")
+			webbrowser.open(self.contenders)
+			#driver.quit()
+		else:
+			Logger.error("Стрим по Overwatch Conteders не найден! Проверю через 5 минут")
+			time.sleep(300)
+			overwatchleague = Overwatch()
+			overwatchleague.contenders_parsing()
+			#driver.quit()
+
+	except Exception as err:
+		Logger.error("Произошла неизвестная ошибка! \n"
+					f"Ошибка - {err} \n"
+					"Открываю саппорт сервер через 3 секунды... \n"
+					"Советуем вам перезапустить код...")
+		time.sleep(3)
+		webbrowser.open(self.support)
+		driver.quit()
+
+	def league_parsing(self):
 		try:
 			options = Options()
 			options.headless = True
@@ -25,13 +56,12 @@ class Overwatch:
 			soup = BeautifulSoup(full_page, "html.parser")
 			items = soup.find_all("h2", {"class": "section-titlestyles__Title-sc-1wlp19u-3", "class": "bJDlju"})
 
-			if items[0].text == "Live now":
-				Logger.info("Стрим Overwatch Contenders найден! Открываю страничку")
-				webbrowser.open(self.contenders)
+			if items[0].text == "Live Now": 
+				Logger.info("Стрим Overwatch League найден! Открываю страничку")
+				webbrowser.open(self.league)
 				#driver.quit()
 			else:
-				Logger.error("Стрим по Overwatch Conteders не найден! Проверю через 5 минут")
-				time.sleep(300)
+				Logger.error("Стрим по Overwatch League не найден!")
 				overwatchleague = Overwatch()
 				overwatchleague.contenders_parsing()
 				#driver.quit()
@@ -44,36 +74,6 @@ class Overwatch:
 			time.sleep(3)
 			webbrowser.open(self.support)
 			driver.quit()
-
-		def league_parsing(self):
-			try:
-				options = Options()
-				options.headless = True
-				driver = webdriver.Firefox(options=options, executable_path=r'C:\webDrivers\geckodriver.exe')
-				driver.get(self.contenders)
-
-				full_page = driver.page_source
-				soup = BeautifulSoup(full_page, "html.parser")
-				items = soup.find_all("h2", {"class": "section-titlestyles__Title-sc-1wlp19u-3", "class": "bJDlju"})
-
-				if items[0].text == "Live Now": 
-					Logger.info("Стрим Overwatch League найден! Открываю страничку")
-					webbrowser.open(self.league)
-					#driver.quit()
-				else:
-					Logger.error("Стрим по Overwatch League не найден!")
-					overwatchleague = Overwatch()
-					overwatchleague.contenders_parsing()
-					#driver.quit()
-
-			except Exception as err:
-				Logger.error("Произошла неизвестная ошибка! \n"
-							f"Ошибка - {err} \n"
-							"Открываю саппорт сервер через 3 секунды... \n"
-							"Советуем вам перезапустить код...")
-				time.sleep(3)
-				webbrowser.open(self.support)
-				driver.quit()
 
 overwatchleague = Overwatch()
 overwatchleague.contenders_parsing()
