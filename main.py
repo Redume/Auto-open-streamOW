8a31f732517427d360631ac2c548732327885d6b
Vasya775 <59001661+Vasya775@users.noreply.github.com>
2021-06-28 15:04:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 import webbrowser
 from win10toast import ToastNotifier
 from utils.logger import Logger
-
+#Здесь был Вася
 class Overwatch:
     	contenders = "https://overwatchleague.com/en-us/contenders"
 	league = "https://overwatchleague.com/en-us/"
